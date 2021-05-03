944a1143faf98eba7389ac93683dcf3523d72c66
beeAk <se.blen.aklilu@gmail.com>
2021-05-03 07:45:39 -0500

    Handle events Use bind to resolve this or use arrow function

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -7,13 +7,19 @@ class Counter extends Component {
         tags: ['tag1','tag2','tag3']
         
     };
+    // constructor() {
+    //     // super()
+    //     // this.handleIncrement = this.handleIncrement.bind(this);
+    // }
+    handleIncrement = () => {
+        console.log('Increment Clicked', this.state.count++)
+    }
     render() { 
         return ( 
         <React.Fragment>
-            {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span> 
-            <button className="btn btn-secondary btn-sm">Increment</button> */} 
-                <div>{this.state.tags.length === 0 && 'Please add Tag' }{this.renderTags()}</div>
-           
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span> 
+            <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button> 
+                
             </React.Fragment>);
     }
     renderTags(){
@@ -21,16 +27,15 @@ class Counter extends Component {
         return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
         
     }
-    // getBadgeClasses() {
-    //     let classes = "badge m-2 badge-";
-    //     classes += this.state.count === 0 ? "warning" : "primary";
-    //     return classes;
-    // }
-
-    // formatCount(){
-    //     const {count} = this.state
-    //     return count === 0 ? 'Zero' : count;
-    // }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+    formatCount(){
+        const {count} = this.state
+        return count === 0 ? 'Zero' : count;
+    }
 }
  
 export default Counter;
