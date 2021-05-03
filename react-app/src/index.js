0d36299a795a204cbabc6c3aa64403817ee4c329
beeAk <se.blen.aklilu@gmail.com>
2021-05-02 21:35:41 -0500

    -setting attributes  -js expressions -render dynamic values

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css'
 import App from './App';
 import "bootstrap/dist/css/bootstrap.css";
+import Counter from './components/counter';
 
 
 
 const element = <h1>Hello World</h1>;
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Counter/>, document.getElementById('root'));
 console.log(element);
