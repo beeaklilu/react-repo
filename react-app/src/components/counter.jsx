7808207875213a1b092d91c1e8a526003647db4e
beeAk <se.blen.aklilu@gmail.com>
2021-05-03 07:48:47 -0500

    change state

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -12,7 +12,7 @@ class Counter extends Component {
     //     // this.handleIncrement = this.handleIncrement.bind(this);
     // }
     handleIncrement = () => {
-        console.log('Increment Clicked', this.state.count++)
+        this.setState({count: this.state.count + 1})
     }
     render() { 
         return ( 
