1e361d08aaa4b5e3959f202a2a4eb4791dba2c02
beeAk <se.blen.aklilu@gmail.com>
2021-05-03 18:44:56 -0500

    Handling and Raising events updating state

--- react-app/src/components/counters.jsx
+++ react-app/src/components/counters.jsx
@@ -10,10 +10,15 @@ class Counters extends Component {
             {id: 4, value: 0},
         ]
      }
+     handleDelete = (counterId) => {
+         const counters = this.state.counters.filter(counter => counter.id !== counterId)
+         this.setState({counters})
+        
+     }
     render() { 
         return ( <div>
            {this.state.counters.map(counter => 
-           <Counter key={counter.id} value={counter.value} selected={true}
+           <Counter key={counter.id} onDelete={this.handleDelete} counter={counter}
            />)}
         </div> );
     }
